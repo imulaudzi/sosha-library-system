ee994a9bbaf90e795845d19becf5ec0eddaf29c8
Isaac Mulaudzi <imulaudzi@gmail.com>
2015-01-14 21:54:23 +0200

    [Sosha Library System] Birth

---
+++ src/main/java/com/library/common/Login.java
@@ -0,0 +1,7 @@
+package com.library.common;
+
+/**
+ * Created by ISAAC on 1/14/2015.
+ */
+public class Login {
+}
---
+++ src/main/java/com/library/student/model/Student.java
@@ -0,0 +1,7 @@
+package com.library.student.model;
+
+/**
+ * Created by ISAAC on 1/14/2015.
+ */
+public class Student {
+}
